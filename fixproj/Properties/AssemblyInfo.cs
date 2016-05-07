5c13d3f4d5b3f17594fd5d44b86bc8af4c4e340c
Kit Cragin <ccragin@eprod.com>
2016-05-07 10:22:27 -0500

    corrected attribution

--- fixproj/Properties/AssemblyInfo.cs
+++ fixproj/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("fixproj")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Originally conceived by Kit Cragin to reduce our team's merge conflict headaches. Enjoy!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Enterprise Products Partners L.P. (Enterprise)")]
 [assembly: AssemblyProduct("fixproj")]
-[assembly: AssemblyCopyright("Copyright (C) Kit Cragin")]
+[assembly: AssemblyCopyright("Copyright (C) 2016 Enterprise Products Partners L.P. (Enterprise)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
